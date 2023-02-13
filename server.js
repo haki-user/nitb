74fcadec60aca7a75fe0846025bcc10dd1bf5299
haki-user <adityapratapsingh33@outlook.com>
2023-02-13 10:30:50 +0530

    modified:   server.js

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const path = require('path')
 const app = express()
-const PORT = 4000
+
+const port = procees.env.PORT || 4000
+const mongoUrl = process.env.MONGODB_URI || 'mongodb://localhost:27017/nitb'
+
 const mongoose = require('mongoose')
 
 const csv = require('csv-parser')
@@ -20,7 +23,7 @@ app.use('public', express.static(__dirname + '/public'))
 
 
 
-mongoose.connect('mongodb://localhost:27017/nitb',{useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
+mongoose.connect(mongoUrl, {useNewUrlParser:true,useUnifiedTopology:true}).then(()=>{
     console.log('mongodb connected')
 }).catch((err)=>{
     console.log(err)
@@ -65,62 +68,6 @@ app.post("/api/v1/fromcsv", (req, res)=>{
   console.log("csv sent....")
 });   
   
-// show
-// app.get('/records.ejs', (req, res)=>{
-//   Record.find({}, (err, records)=>{
-//     if(err) return res.status(500).send(err);
-//     return res.render('records', {records:records});
-//   })
-//   // res.sendFile(path.join(__dirname + '/views/records.ejs'))
-// })
-// app.get('/api/v1/data', function(req, res) {
-//   Record.countDocuments({}, function(err, count) {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send('Error fetching data from MongoDB');
-//     } else {
-//       Record.find({}, function(err, records) {
-//         if (err) {
-//           console.error(err);
-//           res.status(500).send('Error fetching data from MongoDB');
-//         } else {
-//           res.send(`
-//             <html>
-//               <head>
-//                 <title>Records</title>
-//               </head>
-//               <div> <h1>Students Placed: ${count}<h1> </div>
-//               <body>
-//                 <table>
-//                   <thead>
-//                     <tr>
-//                       <th>Student Name</th>
-//                       <th>Gender</th>
-//                       <th>Company</th>
-//                       <th>Package</th>
-//                       <th>Year</th>
-//                     </tr>
-//                   </thead>
-//                   <tbody>
-//                     ${records.map(record => `
-//                       <tr>
-//                         <td>${record.studentName}</td>
-//                         <td>${record.gender}</td>
-//                         <td>${record.company}</td>
-//                         <td>${record.package}</td>
-//                         <td>${record.year}</td>
-//                       </tr>
-//                     `).join('')}
-//                   </tbody>
-//                 </table>
-//               </body>
-//             </html>
-//           `);
-//         }
-//       });
-//     }
-//   });
-// });
 
 app.get('/api/v1/data', function(req, res) {
   let count;
@@ -184,122 +131,7 @@ app.get('/api/v1/data', function(req, res) {
     }
   });
 });
-// app.get('/api/v1/data', function(req, res) {
-//   let count;
-//   Record.countDocuments({}, function(err, count) {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send('Error fetching data from MongoDB');
-//     } else {
-//       // console.log(record)
-//       let maleCount = 0;
-//       let femaleCount = 0;
-//       let maxPackageCompany = '';
-//       let maxPackage = 0;
-//       Record.find({}, function(err, records) {
-//         console.log(records)
-//         records.forEach(record => {
-//           if (record.gender === 'male') {
-//             maleCount++;
-//           } else if (record.gender === 'Female') {
-//             femaleCount++;
-//           }
-//           if (record.package > maxPackage) {
-//             maxPackage = record.package;
-//             maxPackageCompany = record.company;
-//           }
-//         });
-//         res.send(`
-//           <html>
-//             <head>
-//               <title>Records</title>
-//               <script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
-//             </head>
-//             <body>
-//               <div>Total Entries: ${count}</div>
-//               <div>Male: ${maleCount}</div>
-//               <div>Female: ${femaleCount}</div>
-//               <div>Company with Maximum Package: ${maxPackageCompany} (${maxPackage})</div>
-//               <canvas id="myChart"></canvas>
-//               <table>
-//                 <thead>
-//                   <tr>
-//                     <th>Student Name</th>
-//                     <th>Gender</th>
-//                     <th>Company</th>
-//                     <th>Package</th>
-//                     <th>Year</th>
-//                   </tr>
-//                 </thead>
-//                 <tbody>
-//                   ${records.map(record => `
-//                     <tr>
-//                       <td>${record.studentName}</td>
-//                       <td>${record.gender}</td>
-//                       <td>${record.company}</td>
-//                       <td>${record.package}</td>
-//                       <td>${record.year}</td>
-//                     </tr>
-//                   `).join('')}
-//                 </tbody>
-//               </table>
-//               <script>
-//                 let companies = [];
-//                 let packages = [];
-//                 ${records.map(record => `
-//                   companies.push("${record.company}");
-//                   packages.push(${record.package});
-//                 `).join('')}
-//                 let ctx = document.getElementById('myChart').getContext('2d');
-//                 let chart = new Chart(ctx, {
-//                   type: 'bar',
-//                   data: {
-//                     labels: companies,
-//                     datasets: [{
-//                       label: 'Package offered by each company',
-//                       data: packages,
-//                       backgroundColor: 'rgb(255, 99, 132)',
-//                       borderColor: 'rgb(255, 99, 132)',
-//                       borderWidth: 1
-//                     }]
-//                   },
-//                   options: {
-//                     scales: {
-//                       yAxes: [{
-//                         ticks: {
-//                           beginAtZero: true,
-//                           callback: function(value) {
-//                             if (Number.isInteger(value)) {
-//                               return value;
-//                             }
-//                           }
-//                         }
-//                       }],
-//                       xAxes: [{
-//                       barPercentage: 0.4,
-//                       gridLines: {
-//                       display: false
-//                       }
-//                       }]
-//                       }
-//                       };
-                      
-//                       var barChart = new Chart(ctx, {
-//                       type: 'bar',
-//                       data: barData,
-//                       options: barOptions
-//                       });
-                      
-//                       });
-//                       </script>
-//                       </body>  
-//                       </html>
-//                       `);
-//                       });
-//                       }
-//                       });
-//                       });                        
-  
+
   
 app.get('/api/v1/graph', (req, res)=>{
   res.sendFile(path.join(__dirname + '/public/graph.html'))
@@ -314,7 +146,7 @@ app.get('/api/v1/graphData', (req, res)=>{
 
 app.set('view engine', path.join(__dirname, 'views'))
 
-app.listen(PORT, ()=>{
+app.listen(port, ()=>{
   console.log(`ok port ${PORT}`)
 })  
     
